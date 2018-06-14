1e0be96946049f69a865cc326292d0b9bfbb0eb0
SherineAwad <sherine.awad@gmail.com>
2018-06-14 13:05:15 +0100

    update conf.py to add sphinx.ext.autosectionlabel extension

--- conf.py
+++ conf.py
@@ -31,7 +31,9 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.todo']
+    'sphinx.ext.todo', 
+    'sphinx.ext.autosectionlabel'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- lncRNA.rst
+++ lncRNA.rst
@@ -3,7 +3,7 @@
 =============================================
 
       
-Here we will use lncScore, check `setlncscore`_  for installation tips. 
+Here we will use lncScore, check :ref:`setlncscore`_  for installation tips. 
  
 We use MyTranscripts.fa which contains our transcript in fasta format ::
 
