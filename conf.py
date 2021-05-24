30f58e44102f59911e50cbd48ac433e0d17c31ee
EC2 Default User <ec2-user@ip-172-31-23-192.eu-west-2.compute.internal>
2021-05-24 15:38:28 +0000

    Change sphinx theme

--- conf.py
+++ conf.py
@@ -30,7 +30,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_adc_theme', 'sphinx.ext.autodoc',
+extensions = ['sphinx_adc_theme', 
+              'sphinx.ext.autodoc',
     'sphinx.ext.todo', 
     'sphinx.ext.autosectionlabel'
 ]
@@ -85,10 +86,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_adc_theme
-html_theme = 'sphinx_adc_theme'
-html_theme_path = [sphinx_adc_theme.get_html_theme_path()]
-
+html_theme = "sphinx_adc_theme"
+html_theme_path = ["_themes", ]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
